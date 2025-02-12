bdd6a96b2dc76c32b70dfaf0bcc06f4fbebcc33e
Johns <johnsnoble1@gmail.com>
2025-02-12 18:46:13 +0000

    Added Polynomial Composition Algorithm

---
+++ src/CompositionPolynomial.jl
@@ -0,0 +1,30 @@
+function poly_exponent(qs, N)
+    M = length(qs)
+    il, jl = N+1, (N*(M-1)+1)
+    dp = fill(Complex{Int64}(0), il, jl)
+
+    dp[2,1:M] = qs
+    dp[:,1] = qs[1] .^ (0:N)
+    
+    for j in 2:jl
+        qt = qs[min(j, M):-1:2]
+        s = sum(dp[2:il-1,max(1,j-M+1):j-1] .* qt', dims=2)
+        q = qs[1]
+        s[1] += q*dp[2,j]
+        dp[3:il, j] = accumulate((x,y)-> q*x+y, s)
+    end
+    return dp
+end
+
+
+function poly_compose(ps, qs)
+    N = length(ps)
+    coeffs = poly_exponent(qs, N-1)
+    return ps'*coeffs
+end
+
+# Some test code
+# const N = 4
+# const M = 3
+# qs = [rand(0:5, M-1); 1]
+# ps = [rand(0:5, N-1); 1]
