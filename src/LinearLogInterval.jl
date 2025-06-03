7e5c9e919f3067abb26f1a20f27d96c3a41613e0
Johns <johnsnoble1@gmail.com>
2025-06-03 20:51:38 +0100

    Adding the linear log transform

---
+++ src/LinearLogInterval.jl
@@ -0,0 +1,29 @@
+include("../src/Common.jl")
+
+λ = 3+4im
+μ = 2im
+# Returns ∫Pₖ(s)Log(z-(μ+λs))ds
+function transform(k,z,λ,μ)
+    L=get_l_vec((z-μ)/λ,k)
+    L+=getOffset_(k,z,λ,μ)
+    L[1]+=2*log(λ)
+    return L
+end
+
+function getOffset_(k,z,λ,μ)
+    if imag(λ)==0
+        return 0
+    end
+    s = imag(z-μ)/imag(λ)
+    if s<=-1
+        return 0
+    end
+    x = real(z-μ)-s*real(λ)
+    if x>= 0
+        return 0
+    end
+    ultsph = [legendreInt(k_,0) for k_=0:k]
+    ultsph[1] = 2-ultsph[1]
+    ultsph[2:k+1] *= -1
+    return (imag(λ)>0 ? 1 : -1)*2pi*im*ultsph
+end
